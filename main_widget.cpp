66a7e7403450b94bb49517e8956832b28777a847
shenxi11 <859565175@qq.com>
2024-09-30 10:07:55 +0800

    更新

--- main_widget.cpp
+++ main_widget.cpp
@@ -2,21 +2,28 @@
 
 Main_Widget::Main_Widget(QWidget *parent) : QWidget(parent)
   ,w(nullptr)
+    ,list(nullptr)
 {
-    w=new Play_Widget(this);
-    w->setFixedSize(800,500);
-
-    QRegion region(0, 350, w->width(), 350);
-    w->setMask(region);
-
     resize(1000,600);
 
+    w = new Play_Widget(this);
+    w->setFixedSize(800,500);
+    QRegion region(0, 350, w->width(), 350);
+    w->setMask(region);
     w->move((this->width()-w->width())/2,this->height()-w->height());
 
 
+    list = new MusicListWidget(this);
+    list->setFixedSize(200,300);
+    list->move(this->width()-200,0);
+    list->clear();
+    list->close();
+
+    connect(list,&MusicListWidget::selectMusic,w,&Play_Widget::_play_list_music);
+
     connect(w,&Play_Widget::big_clicked,this,[=](bool checked){
         if(checked){
-            for(int i=350;i>=0;i-=10){
+            for(int i = 350; i >= 0;i -= 10){
                 QRegion region1(0, i, w->width(), i);
                 w->setMask(region1);
 //                QThread::msleep(50);
--- main_widget.h
+++ main_widget.h
@@ -1,6 +1,7 @@
 #ifndef TEST_WIDGET_H
 #define TEST_WIDGET_H
 #include"play_widget.h"
+#include "music_list_widget.h"
 #include <QWidget>
 
 class Main_Widget : public QWidget
@@ -14,7 +15,7 @@ signals:
 
 private:
     Play_Widget*w;
-
+    MusicListWidget*list;
 };
 
 #endif // TEST_WIDGET_H
